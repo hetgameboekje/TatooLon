9b9ff732c7893c0df5ce1337431bb63b06fba8a3
HetGameBoekje <bergthaler.timo@gmail.com>
2023-03-25 13:29:48 +0100

    Fixed floating img

--- Front-end/index.php
+++ Front-end/index.php
@@ -30,8 +30,8 @@
         <div class="profileIMG-DIV">
             <img src="assets/css/img/profileimgLON.jpg" class="profileIMG" alt="">
         </div>
-        <div class="facebookButton"><img src="assets/css/img/profileimgLON.jpg" alt=""class="smallIMGbutton"> Testing</div>
-        <div class="instaButton"><img src="assets/css/img/profileimgLON.jpg" alt=""class="smallIMGbutton">Insta</div>
+        <div class="facebookButton"><img src="assets/css/img/profileimgLON.jpg" alt=""class="smallIMGbutton"> Facebook</div>
+        <div class="instaButton"><img src="assets/css/img/profileimgLON.jpg" alt=""class="smallIMGbutton">Instagram</div>
 
     </div>
 
